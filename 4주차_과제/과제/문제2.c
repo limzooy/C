292705ef0c6384c38b85a316536597e07283da30
Lim JY <100983190+limzooy@users.noreply.github.com>
2024-04-01 13:46:20 +0900

    4주차 과제
    프로그래밍 4주차 과제

---
+++ 4주차_과제/과제/문제2.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //stdio.h라는 헤더 파일에 선언되어 있는 내용 출력
+
+int main(void) //정수를 반환하는 main함수를 의미, 실행 시 0 반환
+{
+	printf("Hello\nC\nProgrammers!"); //출력 내용
+
+	return 0; //0으로 반환
+}
---
+++ 4주차_과제/과제/문제4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>//stdio.h라는 헤더 파일에 선언되어 있는 내용 출력
+
+int main(void)//정수를 반환하는 main함수를 의미, 실행 시 0 반환
+{
+	printf("일\t월\t화\t수\t목\t금\t토\n");//\t를 사용해서 탭키를 누른 것처럼 커서를 이동시킴
+	printf("1\t2\t3\t4\t5\t6\t7\n");//출력 내용
+	printf("8\t9\t10\t11\t12\t13\t14\n");//출력 내용
+	printf("15\t16\t17\t18\t19\t20\t21\n");//출력 내용
+	printf("22\t23\t24\t25\t26\t27\t28\n");//출력 내용
+	printf("29\t30\t31");//출력 내용
+
+	return 0;//0으로 반환
+}
